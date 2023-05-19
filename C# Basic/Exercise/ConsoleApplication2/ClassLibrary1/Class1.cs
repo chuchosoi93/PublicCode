a623a9c9f637ad114e7c9242d0d54f79390e675b
tuanloc911 <tran.tuan.loc@usol-v.com.vn>
2023-05-19 11:26:35 +0700

    update

---
+++ C# Basic/Exercise/ConsoleApplication2/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ClassLibrary1
+{
+	public class Class1
+	{
+		public int a = 0;
+		protected internal int b = 0;
+		void A()
+		{
+
+		}
+	}
+}
---
+++ C# Basic/Exercise/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			button1.Text = "123";
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			button1.Focus();
+		}
+
+		private void button1_MouseClick(object sender, MouseEventArgs e)
+		{
+
+		}
+
+		private void button1_TextChanged(object sender, EventArgs e)
+		{
+			MessageBox.Show("1");
+		}
+	}
+}
