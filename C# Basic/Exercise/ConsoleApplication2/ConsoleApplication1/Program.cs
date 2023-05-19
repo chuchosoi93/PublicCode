1f74066219389f4e74e03a02e0eae795b3802505
tuanloc911 <tran.tuan.loc@usol-v.com.vn>
2023-05-19 15:22:36 +0700

    update

---
+++ C# Basic/Exercise/ConsoleApplication2/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibrary1;
+
+namespace ConsoleApplication1
+{
+	class Program: Class1
+	{
+		static void Main(string[] args)
+		{
+			Class1 a = new Class1();
+			
+			
+		}
+	}
+}
---
+++ C# Basic/Exercise/ConsoleApplication2/ConsoleApplication2/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ConsoleApplication2
+{
+
+	class PolymorphDemo
+	{
+		public static void Main()
+		{
+			Child ObjChild = new Child();
+			Console.WriteLine("The output is " + ObjChild.MethodA());
+			Console.ReadLine();
+		}
+
+	}
+
+
+	class Parent
+	{
+		event Display a;
+		public int MethodA()
+		{
+			return (MethodB() * MethodC());
+		}
+		public virtual int MethodB()
+		{
+			return (10);
+		}
+		public int MethodC()
+		{
+			return (20);
+		}
+	}
+
+	class Child : Parent
+	{
+		public override int MethodB()
+		{
+
+			try
+			{
+			}
+			catch(Exception e)
+			{}
+			catch{Exception b}
+
+			return 1;
+		}
+
+		
+
+		
+
+	}
+
+}
+
